c8e871bb1f8005a63ea4e5ca5944f494b787e171
Aaron Cunnington <aaron.cunnington@ripjar.com>
2018-01-09 21:08:38 +0000

    For lib stuff, only bundle our own app code.
    Call it a bundle.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,6 +2,7 @@ import path from "path";
 
 import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import WebpackExternalModule from "webpack-external-module";
 
 const config = {
   entry: {
@@ -10,14 +11,19 @@ const config = {
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name]-dist.js"
+    filename: "[name]-bundle.js"
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src/demo/index.html"),
       chunks: ["demo"]
     }),
-    new webpack.NoEmitOnErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "vendor",
+      filename: "[name]-bundle.js",
+      minChunks: module => WebpackExternalModule.isExternal(module)
+    })
   ],
   module: {
     rules: [
